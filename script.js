88ce794a671f7e3053de7ce1161ee1e4814f12a0
dianatorresrios <134332579+dianatorresrios@users.noreply.github.com>
2023-06-06 19:11:48 -0500

    ARREGLOS  JAVA
    ARREGLOS EN JAVA SCRIPT

--- script.js
+++ script.js
@@ -1,122 +1,31 @@
 //PUNTO UNO//
-let respuesta = prompt("¿Eres bellisimo/a?");
+// Creamos un array vacío
+let numerosAzar = [];
 
-if (respuesta === "sí") {
-  console.log("Ciertamente");
-} else if (respuesta === "no") {
-  console.log("No te creo");
-} else {
-  console.log("Lo siento, no entendí tu respuesta");
+// Generamos 10 números al azar y los agregamos al array
+for (let i = 0; i < 10; i++) {
+  numerosAzar.push(Math.floor(Math.random() * 100) + 1);
 }
+
+// Mostramos el array en consola
+console.log(numerosAzar);
 
 //PUNTO DOS//
-// Pedimos al usuario que ingrese un número
-const numero = prompt('Ingresa un número:');
 
-// Convertimos el valor ingresado a un número entero
-const entero = parseInt(numero);
-
-// Verificamos si el número es divisible por dos
-if (entero % 2 === 0) {
-    alert(`${entero} es divisible entre 2`);
-} else {
-    alert(`${entero} no es divisible entre 2`);
-}
+let palabras = prompt("Ingrese varias palabras separadas por coma:");
+let arrayPalabras = palabras.split(",");
+console.log(arrayPalabras);
 
 //PUNTO TRES//
-var num = prompt("Ingresa un número");
-if (num % 2 == 0) {
-  alert(num + " es par");
-} else {
-  alert(num + " no es par");
-}
-  //PUNTO CUATRO//
-   
-let numCliente = prompt('Por favor, ingrese su número de cliente:');
 
-if (numCliente === '1000') {
-  console.log('Felicitaciones, ganaste un premio');
-} else {
-  console.log(`Lo sentimos, su número de cliente es ${numCliente}. Sigue participando!`);
-}
+const arr = [10, 40, 30, 20, 15, 5];
 
-//PUNTO CINCO//
-// Pedir al usuario que ingrese dos números
-let num1 = prompt("Ingresa el primer número:");
-let num2 = prompt("Ingresa el segundo número:");
+// Ordenar el arreglo de menor a mayor
+arr.sort((a, b) => a - b);
+console.log(`Arreglo ordenado de menor a mayor: ${arr}`);
 
-// Convertir los números a tipo numérico
-num1 = Number(num1);
-num2 = Number(num2);
-
-// Verificar cuál de los dos números es menor
-if (num1 < num2) {
-  console.log("El primer número es menor: " + num1);
-} else {
-  console.log("El segundo número es menor: " + num2);
-}
-
-//PUNTO SEIS//
-
-let num1 = parseInt(prompt("Ingrese el primer número: "));
-let num2 = parseInt(prompt("Ingrese el segundo número: "));
-let num3 = parseInt(prompt("Ingrese el tercer número: "));
-
-if (num1 > num2 && num1 > num3) {
-  console.log(`El número más grande es ${num1}`);
-} else if (num2 > num1 && num2 > num3) {
-  console.log(`El número más grande es ${num2}`);
-} else if (num3 > num1 && num3 > num2) {
-  console.log(`El número más grande es ${num3}`);
-} else {
-  console.log("Hay dos o tres números iguales");
-}
-
-//PUNTO SEPTIMO//
-let day = prompt("Ingresa un día de la semana");
-
-if (day === "lunes") {
-  console.log("Buen comienzo de semana");
-} else if (day === "viernes") {
-  console.log("Feliz viernes");
-} else if (day === "sábado" || day === "domingo") {
-  console.log("Buen fin de semana");
-} else {
-  console.log("El día ingresado no es válido");
-}
-
-//PUNTO OCTAVO//
-// Pedimos al usuario que ingrese una calificación
-let calificacion = parseInt(prompt("Ingresa tu calificación (entre 1 y 10):"));
-
-// Comprobamos si la calificación está dentro del rango correcto
-if (calificacion >= 1 && calificacion <= 10) {
-
-  // Imprimimos un mensaje dependiendo de la calificación ingresada
-  if (calificacion < 6) {
-    console.log("Reprobado");
-  } else if (calificacion >= 6 && calificacion <= 8) {
-    console.log("Regular");
-  } else if (calificacion === 9) {
-    console.log("Bien");
-  } else {
-    console.log("Excelente");
-  }
-
-} else {
-  console.log("Error: la calificación debe estar entre 1 y 10");
-}
-
-
-
-
-
-
-
-
-
-
-    
-    
-
-    
+// Obtener el número menor y mayor del arreglo
+const min = Math.min(...arr);
+const max = Math.max(...arr);
+console.log(`Número menor del arreglo: ${min}`);
+console.log(`Número mayor del arreglo: ${max}`);
